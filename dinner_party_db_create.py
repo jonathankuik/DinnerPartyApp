27bf94b7cadeba683184fd000a43e85b72e43346
Jonathan Kuik <jonathankuik@gmail.com>
2017-02-22 23:37:25 +0000

    imported os

--- dinner_party_db_create.py
+++ dinner_party_db_create.py
@@ -2,6 +2,7 @@
 ###################Configuration Code########################
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
  
 app = Flask(__name__)
 db = SQLAlchemy(app)
