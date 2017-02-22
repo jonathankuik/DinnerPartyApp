7816aa4503baf4005965bf5b3c02645882521f5f
Jonathan Kuik <jonathankuik@gmail.com>
2017-02-22 23:35:13 +0000

    changed db_create to have the Database URL

--- dinner_party_db_create.py
+++ dinner_party_db_create.py
@@ -6,6 +6,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
 #####################Class Code##############################
 
 class Appetizer(db.Model):
